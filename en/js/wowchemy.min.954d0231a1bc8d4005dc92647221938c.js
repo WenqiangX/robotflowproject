5cb0389e1f36d181b39da4605c1476444bab4d42
WenqiangX <vinjohn@sjtu.edu.cn>
2021-05-09 03:48:53 +0800

    first commit of robotflow project page

--- en/js/wowchemy.min.954d0231a1bc8d4005dc92647221938c.js
+++ en/js/wowchemy.min.954d0231a1bc8d4005dc92647221938c.js
@@ -475,7 +475,7 @@
       clearTimeout(v), v = setTimeout(D, 200);
     });
   })(), (() => {
-    var e = {authors: "Authors", event: "Events", post: "Posts", project: "Projects", publication: "Publications", slides: "Slides"}, f = {no_results: "No results found", placeholder: "Search...", results: "results found"}, c = {indexURI: "/index.json", minLength: 1, threshold: 0.3}, d = {shouldSort: true, includeMatches: true, tokenize: true, threshold: c.threshold, location: 0, distance: 100, maxPatternLength: 32, minMatchCharLength: c.minLength, keys: [{name: "title", weight: 0.99}, {name: "summary", weight: 0.6}, {name: "authors", weight: 0.5}, {name: "content", weight: 0.2}, {name: "tags", weight: 0.5}, {name: "categories", weight: 0.5}]}, a = 60;
+    var e = {authors: "Authors", event: "Events", post: "Posts", project: "Projects", publication: "Publications", slides: "Slides"}, f = {no_results: "No results found", placeholder: "Search...", results: "results found"}, c = {indexURI: "/robotflowproject/index.json", minLength: 1, threshold: 0.3}, d = {shouldSort: true, includeMatches: true, tokenize: true, threshold: c.threshold, location: 0, distance: 100, maxPatternLength: 32, minMatchCharLength: c.minLength, keys: [{name: "title", weight: 0.99}, {name: "summary", weight: 0.6}, {name: "authors", weight: 0.5}, {name: "content", weight: 0.2}, {name: "tags", weight: 0.5}, {name: "categories", weight: 0.5}]}, a = 60;
     function g(a2) {
       return decodeURIComponent((location.search.split(a2 + "=")[1] || "").split("&")[0]).replace(/\+/g, " ");
     }
